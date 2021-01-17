3e06d9f9282a543c8be85301eddaeb8c7b476bce
leshicorn <romanenkov2003@gmail.com>
2021-01-17 10:08:19 +0000

    validator upgraded

--- src/card_num_validator/validator.c
+++ src/card_num_validator/validator.c
@@ -1,28 +1,33 @@
-#include <stdio.h>
-
-#include "string.h"
-
 int validator(char *num)
 {
     int len = 0;
     int sum_odd = 0;
+    int sum_odd1 = 0;
 	int sum_even = 0;
+    int sum_even1 = 0;
 	int checker = 0;
 
 // 1234 4567 7890 1234
 
 	while (*num)
 	{
-		checker = (*num - '0') * 2;
-        checker = (checker > 9) ? checker - 9 : checker;
-		if (!(len % 2))
-			sum_odd += checker;
-		else
-			sum_even += checker;
-		++num;
 		++len;
+		checker = (*num - '0') * 2;
+		printf("%c - %d - %d\n", *num, len, checker);
+		if (len % 2)
+        {
+			sum_odd += (checker > 9) ? checker - 9 : checker;
+            sum_even1 += checker / 2;
+        }
+        else
+		{
+			sum_even += (checker > 9) ? checker - 9 : checker;
+			sum_odd1 += checker / 2;
+		}
+		++num;
 	}
-	printf("%d\n", ((len % 2) ? sum_even : sum_odd) % 10);
-
-    return ((len % 2) ? sum_even : sum_odd) % 10;
+	printf("%d %d\n", sum_odd + sum_even1, sum_even + sum_odd1);
+    return (((len % 2) ?
+			sum_odd + sum_even1 :
+			sum_even + sum_odd1) % 10) == 0;
 }
